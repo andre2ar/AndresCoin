318d73c15274de30ed10b03b319a7e83ef24048e
Andre Alvim Ribeiro <andre2ar@outlook.com>
2020-03-24 10:09:30 -0300

    Verification

--- blockchain.py
+++ blockchain.py
@@ -2,9 +2,10 @@ from functools import reduce
 import pickle
 
 # Import two functions from our hash_util.py file. Omit the ".py" in the import
-from hash_util import hash_string_256, hash_block
+from hash_util import hash_block
 from block import Block
 from transaction import Transaction
+from verification import Verification
 
 # The reward we give to miners (for creating a new block)
 MINING_REWARD = 10
@@ -53,27 +54,6 @@ def save_data():
         print("Blockchain could not been saved!")
 
 
-def valid_proof(transactions, last_hash, proof):
-    """Validate a proof of work number and see if it solves the puzzle algorithm (two leading 0s)
-
-    Arguments:
-        :transactions: The transactions of the block for which the proof is created.
-        :last_hash: The previous block's hash which will be stored in the current block.
-        :proof: The proof number we're testing.
-    """
-    # Create a string with all the hash inputs
-    guess = (str([tx.to_ordered_dict() for tx in transactions]) + str(last_hash) + str(proof)).encode()
-
-    # Hash the string IMPORTANT: This is NOT the same hash as will be stored in the previous_hash. It's a not a
-    # block's hash. It's only used for the proof-of-work algorithm.
-    guess_hash = hash_string_256(guess)
-
-    # Only a hash (which is based on the above inputs) which starts with two 0s is treated as valid This condition is
-    # of course defined by you. You could also require 10 leading 0s - this would take significantly longer (and this
-    # allows you to control the speed at which new blocks can be added)
-    return guess_hash[0:2] == '00'
-
-
 def proof_of_work():
     """Generate a proof of work for the open transactions, the hash of the previous block and a random number (which
     is guessed until it fits). """
@@ -81,7 +61,8 @@ def proof_of_work():
     last_hash = hash_block(last_block)
     proof = 0
     # Try different PoW numbers and return the first valid one
-    while not valid_proof(open_transactions, last_hash, proof):
+    verifier = Verification()
+    while not verifier.valid_proof(open_transactions, last_hash, proof):
         proof += 1
     return proof
 
@@ -102,14 +83,14 @@ def get_balance(participant):
                       for tx in open_transactions if tx.sender == participant]
     tx_sender.append(open_tx_sender)
     amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
-                         if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
+    if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
     # This fetches received coin amounts of transactions that were already included in blocks of the blockchain We
     # ignore open transactions here because you shouldn't be able to spend coins before the transaction was confirmed
     # + included in a block
     tx_recipient = [[tx.amount for tx in block.transactions
                      if tx.recipient == participant] for block in blockchain]
     amount_received = reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)
-                             if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
+    if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
     # Return the total balance
     return amount_received - amount_sent
 
@@ -120,15 +101,6 @@ def get_last_blockchain_value():
         return None
     return blockchain[-1]
 
-
-def verify_transaction(transaction):
-    """Verify a transaction by checking whether the sender has sufficient coins.
-
-    Arguments:
-        :transaction: The transaction that should be verified.
-    """
-    sender_balance = get_balance(transaction.sender)
-    return sender_balance >= transaction.amount
 
 # This function accepts two arguments.
 # One required one (transaction_amount) and one optional one (last_transaction)
@@ -144,7 +116,8 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         :amount: The amount of coins sent with the transaction (default = 1.0)
     """
     transaction = Transaction(sender, recipient, amount)
-    if verify_transaction(transaction):
+    verifier = Verification()
+    if verifier.verify_transaction(transaction, get_balance):
         open_transactions.append(transaction)
         save_data()
         return True
@@ -193,24 +166,6 @@ def print_blockchain_elements():
         print('-' * 20)
 
 
-def verify_chain():
-    """ Verify the current blockchain and return True if it's valid, False otherwise."""
-    for (index, block) in enumerate(blockchain):
-        if index == 0:
-            continue
-        if block.previous_hash != hash_block(blockchain[index - 1]):
-            return False
-        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
-            print('Proof of work is invalid')
-            return False
-    return True
-
-
-def verify_transactions():
-    """Verifies all open transactions."""
-    return all([verify_transaction(tx) for tx in open_transactions])
-
-
 waiting_for_input = True
 
 # A while loop for the user input interface
@@ -223,6 +178,7 @@ while waiting_for_input:
     print('4: Check transaction validity')
     print('q: Quit')
     user_choice = get_user_choice()
+    verifier = Verification()
     if user_choice == '1':
         tx_data = get_transaction_value()
         recipient, amount = tx_data
@@ -238,7 +194,7 @@ while waiting_for_input:
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
-        if verify_transactions():
+        if verifier.verify_transactions(open_transactions, get_balance):
             print('All transactions are valid')
         else:
             print('There are invalid transactions')
@@ -247,7 +203,8 @@ while waiting_for_input:
         waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
-    if not verify_chain():
+
+    if not verifier.verify_chain(blockchain):
         print_blockchain_elements()
         print('Invalid blockchain!')
         # Break out of the loop
@@ -255,6 +212,5 @@ while waiting_for_input:
     print('Balance of {}: {:6.2f}'.format(owner, get_balance(owner)))
 else:
     print('User left!')
-
 
 print('Done!')
